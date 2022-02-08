43f20409e05504a07d3dd33dd12dfa70777f6ad1
Ярослав <92985624+yaroslaw74@users.noreply.github.com>
2022-02-08 12:53:34 +0200

    Create mod_advanced_facebook_likebox.php

---
+++ mod_advanced_facebook_likebox.php
@@ -0,0 +1,13 @@
+<?php
+/*------------------------------------------------------------------------
+# mod_advanced_facebook_likebox - Advanced Facebook Like Box
+# ------------------------------------------------------------------------
+# @author      Kataev Yaroslav
+# @description Facebook like box placement module on the website page.
+# @copyright   (C) 2022 Kataev Yaroslav. All rights reserved.
+# @license     GNU General Public License version 2 or later
+-------------------------------------------------------------------------*/
+
+// no direct access
+defined( '_JEXEC' ) or die;
+require JModuleHelper::getLayoutPath('mod_advanced_facebook_likebox', $params->get('layout'));
