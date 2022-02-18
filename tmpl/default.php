3560328ba9b68cc7aaee29565ffd711cbb03c2f3
Ярослав <92985624+yaroslaw74@users.noreply.github.com>
2022-02-18 12:08:12 +0200

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,13 +1,13 @@
 <?php
-/*------------------------------------------------------------------------
+/*---------------------------------------------------------------------------------------
 # mod_advanced_facebook_likebox - Advanced Facebook Like Box
-# ------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
 # @author      Kataev Yaroslav
 # @version     1.5.0
 # @description Facebook like box placement module on the website page.
 # @copyright   Copyright (C) 2022 Kataev Yaroslav. All rights reserved.
 # @license     license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html; see LICENSE.txt
--------------------------------------------------------------------------*/
+------------------------------------------------------------------------------------------*/
 
 // no direct access
 defined( '_JEXEC' ) or die;
