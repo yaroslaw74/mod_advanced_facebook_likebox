3553899ec88df270ed84275d2f506bd440375a1f
Ярослав <92985624+yaroslaw74@users.noreply.github.com>
2022-02-21 11:11:07 +0200

    Add files via upload

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,32 +1,36 @@
 <?php
-/*---------------------------------------------------------------------------------------
-# mod_advanced_facebook_likebox - Advanced Facebook Like Box
-# ---------------------------------------------------------------------------------------
-# @author      Kataev Yaroslav
-# @version     1.5.0
-# @description Facebook like box placement module on the website page.
-# @copyright   Copyright (C) 2022 Kataev Yaroslav. All rights reserved.
-# @license     license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html; see LICENSE.txt
-------------------------------------------------------------------------------------------*/
+/**
+*-------------------------------------------------------------------------------------------
+* mod_advanced_facebook_likebox - Advanced Facebook Like Box
+*-------------------------------------------------------------------------------------------
+* @author      Kataev Yaroslav
+* @version     1.6.0
+* @description Facebook like box placement module on the website page.
+* @copyright   Copyright (C) 2022 Kataev Yaroslav. All rights reserved.
+* @license     license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html; see LICENSE.txt
+*-------------------------------------------------------------------------------------------
+**/
 
 // no direct access
 defined( '_JEXEC' ) or die;
 
-$href		= trim($params->get('href'));
-$pageName	= stripslashes($params->get('pageName'));
-$width		= trim($params->get('width'));
-$height		= trim($params->get('height'));
-$lang		= $params->get('lang');
+$href = trim($params->get('href', 'http://www.facebook.com/Facebook'));
+$pageName = stripslashes($params->get('pageName', 'Facebook Developers'));
+$width = intval($params->get('width', 340));
+$height = intval($params->get('height', 500));
+$lang		= $params->get('lang', 'en_GB');
 
-$showFaces			= (boolean)$params->get('showFaces');
-$tabsTimeline			= (boolean)$params->get('tabsTimeline');
-$tabsEvents			= (boolean)$params->get('tabsEvents');
-$tabsMessage			= (boolean)$params->get('tabsMessages');
-$showHeader			= (boolean)$params->get('showHeader');
-$adaptContainerWidth		= (boolean)$params->get('adaptContainerWidth');
-$smallHeader			= (boolean)$params->get('smallHeader');
-$hideCta			= (boolean)$params->get('hideCta');
-$moduleCenterHorizontally	= (boolean)$params->get('moduleCenterHorizontally');
+$showFaces = (boolean)$params->get('showFaces', '1');
+$tabsTimeline = (boolean)$params->get('tabsTimeline', '0');
+$tabsEvents = (boolean)$params->get('tabsEvents', '0');
+$tabsMessage = (boolean)$params->get('tabsMessages', '0');
+$showHeader = (boolean)$params->get('showHeader', '0');
+$adaptContainerWidth = (boolean)$params->get('adaptContainerWidth', '1');
+$smallHeader = (boolean)$params->get('smallHeader', '0');
+$hideCta = (boolean)$params->get('hideCta', '0');
+$moduleCenterHorizontally = (boolean)$params->get('moduleCenterHorizontally', '0');
+
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 
 $dataTabs = "";
 if ($tabsTimeline) {$dataTabs = "timeline";}
@@ -42,6 +46,12 @@ if ($width < 180) {$width = 180;}
 if ($width > 500) {$width = 500;}
 if ($height < 70) {$height = 70;}
 	    
+if (!empty($moduleclass_sfx) or $moduleCenterHorizontally) {
+	$divClass = "<div";
+	if ($moduleCenterHorizontally) {$divClass .= ' align="center"';}
+	if (!empty($moduleclass_sfx)) {$divClass .= ' class="'.$moduleclass_sfx.'"';}
+	$divClass .= ">";
+}
 ?>
 <div id="fb-root"></div>
 <script>
@@ -49,18 +59,18 @@ if ($height < 70) {$height = 70;}
   		var js, id = 'facebook-jssdk'; 
 		if (d.getElementById(id)) {return;}
 		js = d.createElement('script'); 
-		js.id = id; 
+		js.id = id;
 		js.async = true;
 		js.src = "https://connect.facebook.net/<?php echo $lang; ?>/sdk.js#xfbml=1&version=v12.0";
 		d.getElementsByTagName('head')[0].appendChild(js);
 	}(document));
 </script>
 <?php
-if ($moduleCenterHorizontally) {echo '<div align="center">';}
+if (!empty($moduleclass_sfx) or $moduleCenterHorizontally) {echo $divClass;}
 ?>
 <div class="fb-page" data-href="<?php echo $href; ?>" data-tabs="<?php echo $dataTabs ?>" data-width="<?php echo $width; ?>" data-height="<?php echo $height; ?>" data-small-header="<?php echo $smallHeader ?>" data-adapt-container-width="<?php echo $adaptContainerWidth ?>" data-hide-cover="<?php echo $showHeader; ?>" data-show-facepile="<?php echo $showFaces; ?>">
 	<blockquote cite="<?php echo $href; ?>" class="fb-xfbml-parse-ignore"><a href="<?php echo $href; ?>"><?php echo $pageName; ?></a></blockquote>
 </div>
 <?php
-if ($moduleCenterHorizontally) {echo '</div>';}
+if (!empty($moduleclass_sfx) or $moduleCenterHorizontally) {echo '</div>';}
 ?>
